d60455bbfe3a80b897b1b6d8e0bc3998270afb0e
rajpatel0 <raj.riya.patel@gmail.com>
2021-12-07 00:20:04 -0500

    Done day 7, probably a more elegant solution exists

---
+++ day7/day7.py
@@ -0,0 +1,22 @@
+import math
+with open('input.txt','r') as f:
+    data = f.readlines()
+
+dataInt = [int(val) for val in data[0].split(',')]
+
+minCost = float('inf')
+for val in range(min(dataInt),max(dataInt)):
+    gasCost = sum([abs(val-i) for i in dataInt])
+    minCost = min(gasCost,minCost)
+print(minCost)
+
+#day2
+
+minCost = float('inf')
+for val in range(min(dataInt),max(dataInt)):
+    gasCost = sum([(abs(val-i)*(abs(val-i)+1))/2 for i in dataInt])
+    minCost = min(gasCost,minCost)
+print(minCost)
+
+
+
